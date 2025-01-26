42366a7d7464c48bcb657fba17519f324df4048e
Karrass Phiri <phirikarrass@gmail.com>
2025-01-26 05:35:41 +0200

    Vercel deployment issue-next.config.ts changes

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from 'next';
+//import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+//const nextConfig: NextConfig = {
   /* config options here */
-};
+//};
 
-export default nextConfig;
+//export default nextConfig;
+ import type { NextConfig } from 'next';
+ 
+  const nextConfig: NextConfig = {
+    /* config options here */
+    //experimental: {
+    //  ppr: 'incremental',
+    //},
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+   
+  export default nextConfig;
+  
